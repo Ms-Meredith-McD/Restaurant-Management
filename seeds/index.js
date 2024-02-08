303a3a64ed26eb061390b6929b81905a69ade898
helm0127 <helm0127@morris.umn.edu>
2024-02-08 14:22:50 -0600

    added seed files

---
+++ seeds/index.js
@@ -0,0 +1,17 @@
+const sequelize = require('../config/connection');
+const seedCustomer = require('./customerData');
+const seedMenu = require('./menuData');
+const seedReservation = require('./reservationData');
+const seedOrder = require('./OrderData');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedGallery();
+
+  await seedPaintings();
+
+  process.exit(0);
+};
+
+seedAll();
