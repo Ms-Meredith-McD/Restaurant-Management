655b896f974cf860856c49beba5807c775097801
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-02-09 11:35:07 -0600

    completed reservation routes need to test next

--- controllers/api/reservationRoutes.js
+++ controllers/api/reservationRoutes.js
@@ -1,42 +1,66 @@
-const router = require('express').Router();
-const Reservations = require('../../models/Reservations');
+onst router = require('express').Router();
+const Book = require('../../models/Reservations');
 
 // GET ALL
 router.get('/', (req, res) => {
     Reservations.findAll().then((reservationData) => {
         res.json(reservationData);
+    })
+    .catch((err) => {
+        res.json(err);
     });
 });
 
 
-// CUSTOMIZED FIND 
-// router.get('/:id', (req, res) => {
-//     Reservations.findAll()
-
-//     }).then((reservationData) => {
-//         res.json(reservationData);
-//     });
-
-
-
-
 // GET BY ID
-// router.get('/:id', (req, res) => {
-//     Book.findByPk(req.params.id).then((bookData) => {
-//         res.json(bookData);
-//     });
-// });
-
-
+router.get('/:id', (req, res) => {
+    Reservations.findByPk(req.params.id).then((reservationData) => {
+        res.json(reservationData);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
+});
 
 
 // CREATE
-// router.post('/', (req, res) => {
-//     Book.create(req.body)
-//         .then((newBook) => {
-//             res.json(newBook);
-//         })
-//         .catch((err) => {
-//             res.json(err);
-//         });
-// });
+router.post('/', (req, res) => {
+    Reservations.create(req.body)
+        .then((newReservation) => {
+            res.json(newReservation);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+// UPDATE 
+router.put('/:id', (req, res) => {
+    Reservations.update(
+        req.body,
+        {
+            where: {
+            id: req.params.isbn,
+            },
+        }
+    )
+        .then((updatedReservation) => {
+            res.json(updatedReservation);
+        })
+        .catch((err) => res.json(err));
+});
+
+
+
+// DELETE
+router.delete('/:id', (req, res) => {
+    Reservation.destroy({
+        where: {
+            id: req.params.id,
+        },
+    })
+        .then((deletedReservation) => {
+            res.json(deletedReservation);
+        })
+        .catch((err) => res.json(err));
+});
