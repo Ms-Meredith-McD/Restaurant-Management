c40b80ec828dfc2e054489b7a291eec52310c722
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-02-09 11:58:31 -0600

    grabbing main

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
-const customerRoutes = require('./customerRoutes');
-const menuRoutes = require('./menuRoutes');
+// const customerRoutes = require('./customerRoutes');
+// const menuRoutes = require('./menuRoutes');
 const orderRoutes = require('./orderRoutes');
-const reservationsRoutes = require('./reservationRoutes');
+// const reservationRoutes = require('./reservationRoutes');
 
-router.use('/customers', customerRoutes);
-router.use('/menu', menuRoutes);
-router.use('/menu', orderRoutes);
-router.use('/menu', reservationsRoutes);
+// router.use('/customer', customerRoutes);
+// router.use('/menu', menuRoutes);
+router.use('/order', orderRoutes);
+// router.use('/reservation', reservationRoutes);
 
 module.exports = router;
