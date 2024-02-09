da3be339cec085f1a63a0b11fbbf7338d06ca8ca
RB <jsriley24505@gmail.com>
2024-02-09 14:13:15 -0600

    fixed merge issues and uncommented a line in index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 // const customerRoutes = require('./customerRoutes');
 const menuRoutes = require('./menuRoutes');
 // const orderRoutes = require('./orderRoutes');
-// const reservationRoutes = require('./reservationRoutes');
+const reservationRoutes = require('./reservationRoutes');
 
 // router.use('/customers', customerRoutes);
 router.use('/menu', menuRoutes);
