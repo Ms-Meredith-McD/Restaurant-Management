2dd325b01c2fbabfb5ccc2c28936da9af795ed6b
RB <jsriley24505@gmail.com>
2024-02-09 14:07:48 -0600

    added a route for the menu

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
-const customerRoutes = require('./customerRoutes');
+// const customerRoutes = require('./customerRoutes');
 const menuRoutes = require('./menuRoutes');
-const orderRoutes = require('./orderRoutes');
-const reservationRoutes = require('./reservationRoutes');
+// const orderRoutes = require('./orderRoutes');
+// const reservationRoutes = require('./reservationRoutes');
 
-router.use('/customers', customerRoutes);
+// router.use('/customers', customerRoutes);
 router.use('/menu', menuRoutes);
-router.use('/order', orderRoutes);
-router.use('/reservations', reservationRoutes);
+// router.use('/order', orderRoutes);
+// router.use('/reservations', reservationRoutes);
 
 module.exports = router;
