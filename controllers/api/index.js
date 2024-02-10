20d5eebd117f79b1dc9e519595edc00509132e6a
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-02-09 20:52:47 -0600

    trying to figure out how to pop multiple menu items in order

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const customerRoutes = require('./customerRoutes');
 const menuRoutes = require('./menuRoutes');
 const orderRoutes = require('./orderRoutes');
-const reservationsRoutes = require('./reservationRoutes');
+const reservationRoutes = require('./reservationRoutes');
 
 router.use('/customer', customerRoutes);
 router.use('/menu', menuRoutes);
