782d8c777c0c7018284c4d9e3aae736ba6218419
RB <jsriley24505@gmail.com>
2024-02-10 19:00:30 -0600

    began to add the javascript logic to reservations put request

---
+++ public/js/reservations.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', async function(event) {
+    event.preventDefault()
+
+    const name = document.querySelector(`input[name="name"]`).value;
+    const tableSize = document.querySelector(`input[name="tableSize"]`).value;
+    const reservationTime = document.querySelector(`input[name="reservationTime"]`).value;
+    const notes = document.querySelector(`input[name="notes"]`).value;
+
+  const resp = await fetch("/api/reservations", {
+      method: "POST",
+      body: JSON.stringify({
+        party_size: tableSize,
+        reservation_datetime: reservationTime,
+        notes: notes,
+        customer_id: req.params.id
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    const data = resp.json
+    console.log(data)
+  })
+  
