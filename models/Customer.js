861541e9a92b53b3c630f092527e3ddb569b097e
helm0127 <helm0127@morris.umn.edu>
2024-02-11 20:34:48 -0600

    fixed logic for signup, didn't test login

--- models/Customer.js
+++ models/Customer.js
@@ -32,7 +32,7 @@ Customer.init(
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [8],
+                len: [5],
             },
         },
         is_manager: {
