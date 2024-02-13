631c83c43f98f0b5b9c8932dab7ef393786ca177
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-02-13 12:24:37 -0600

    dskfsdjlf

--- public/js/manager.js
+++ public/js/manager.js
@@ -16,3 +16,20 @@ document.querySelectorAll('.delete-btn').forEach(button => {
   this.parentElement.remove()
   })
 })  
+
+// document.querySelectorAll('.order-delete').forEach(button => {
+//   button.addEventListener('click', async function() {
+//     const id = this.getAttribute('id')
+
+
+//   const resp = await fetch(`/api/order/` + id, {
+//       method: "DELETE",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//     })
+//     const data = await resp.json()
+//     console.log(data)
+//   this.parentElement.remove()
+//   })
+// })  
