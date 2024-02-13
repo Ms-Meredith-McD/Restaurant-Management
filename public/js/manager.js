700c1fee37aea8529b26ae3be2dddf4d0929ba1d
RB <jsriley24505@gmail.com>
2024-02-13 12:57:35 -0600

    fixed a syntax error in the code preventing the api delete call

--- public/js/manager.js
+++ public/js/manager.js
@@ -1,5 +1,3 @@
-
-
 document.querySelectorAll('.delete-btn').forEach(button => {
   button.addEventListener('click', async function() {
     const id = this.getAttribute('data-id')
@@ -16,3 +14,37 @@ document.querySelectorAll('.delete-btn').forEach(button => {
   this.parentElement.remove()
   })
 })  
+
+document.querySelectorAll('.order-delete').forEach(button => {
+  button.addEventListener('click', async function() {
+    const id = this.getAttribute('data-id')
+
+
+  const resp = await fetch(`/api/order/` + id, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+    const data = await resp.json()
+    console.log(data)
+  this.parentElement.remove()
+  })
+})
+
+document.querySelectorAll('.cust-delete').forEach(button => {
+  button.addEventListener('click', async function() {
+    const id = this.getAttribute('id')
+
+
+  const resp = await fetch(`/api/customer/` + id, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+    const data = await resp.json()
+    console.log(data)
+  this.parentElement.remove()
+  })
+})
