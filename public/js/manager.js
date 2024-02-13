41c0c7e4ff0d7e1e936cb6bf731e8582453c7a0c
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-02-13 14:00:57 -0600

    prepping for pull

--- public/js/manager.js
+++ public/js/manager.js
@@ -19,10 +19,27 @@ document.querySelectorAll('.delete-btn').forEach(button => {
 
 // document.querySelectorAll('.order-delete').forEach(button => {
 //   button.addEventListener('click', async function() {
-//     const id = this.getAttribute('id')
+//     const id = this.getAttribute('data-id')
 
 
 //   const resp = await fetch(`/api/order/` + id, {
+//       method: "DELETE",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//     })
+//     const data = await resp.json()
+//     console.log(data)
+//   this.parentElement.remove()
+//   })
+// })  
+
+// document.querySelectorAll('.cust-delete').forEach(button => {
+//   button.addEventListener('click', async function() {
+//     const id = this.getAttribute('id')
+
+
+//   const resp = await fetch(`/api/customer/` + id, {
 //       method: "DELETE",
 //       headers: {
 //         "Content-Type": "application/json"
